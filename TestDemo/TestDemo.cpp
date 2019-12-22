a2e6e3c7f7876d6703159d60b4ef4da412930595
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2019-12-22 17:58:39 +0800

    add model util

--- TestDemo/TestDemo.cpp
+++ TestDemo/TestDemo.cpp
@@ -1,12 +1,49 @@
 #include "TestDemo.h"
 #include "../Mx/GameObject/MxGameObject.h"
+#include "../Mx/Resource/MxResourceLoader.h"
+#include "../Mx/Resource/Model/MxModel.h"
+#include "../Mx/Scene/MxScene.h"
+#include "../Mx/Scene/MxSceneManager.h"
+#include "../Mx/Component/Camera/MxCamera.h"
+#include "../Mx/Component/Renderer/MxRenderer.h"
+#include "../Mx/Graphics/MxGraphics.h"
+#include "../Mx/Graphics/MxMaterial.h"
+#include "../Mx/Graphics/Texture/MxTexture.h"
+
 #include "Assets/Scripts/TestScript/TestScript.h"
+
+HGameObject TestDemo::CreateGameObjFromModel(const std::string& _name,
+                                             const std::string& _modelPath,
+                                             const std::vector<std::string>& _texturePaths) {
+    auto obj = ResourceLoader::Get()->load<Model>(_modelPath)->genDefaultGameObjects(_name);
+
+    std::vector<std::shared_ptr<Material>> mats;
+    mats.reserve(_texturePaths.size());
+    for(auto& path : _texturePaths) {
+        auto mat = std::make_shared<Material>(Graphics::Get()->findShader("Standard"));
+        mat->setTexture("diffuseTex", ResourceLoader::Get()->load<Texture>(path));
+        mats.emplace_back(std::move(mat));
+    }
+
+    obj->addComponent<Renderer>()->setMaterials(std::move(mats));
+    return obj;
+}
 
 std::string TestDemo::getAppName() const { return std::string("TestDemo"); }
 
 Version TestDemo::getAppVersion() const { return Version(1, 0, 0); }
 
 void TestDemo::onMainSceneCreated() {
-    auto obj = GameObject::Instantiate("Hz NB");
-    obj->addComponent<TestScript>();
+    auto player = CreateGameObjFromModel("reimu",
+                                         "TestDemo/Assets/Models/reimu/reimu.gltf",
+                                         {
+                                             "TestDemo/Assets/Models/reimu/te3.png",
+                                             "TestDemo/Assets/Models/reimu/te1.png",
+                                             "TestDemo/Assets/Models/reimu/te2.png"
+                                         });
+    player->transform().translate(0, 0, 5);
+
+    auto camera = SceneManager::Get()->getActiveScene()->getMainCamera();
+    camera->transform()->setPosition(Vector3f::Zero);
+    camera->transform()->setRotation(Quaternion::LookRotation(Vector3f::Forward));
 }
--- TestDemo/TestDemo.h
+++ TestDemo/TestDemo.h
@@ -4,11 +4,16 @@
 #define TESTDEMO_H_
 
 #include "../MxApplicationBase.h"
+#include "../Mx/Definitions/MxDefinitions.h"
 
 using namespace Mix;
 
 class TestDemo final : public ApplicationBase {
 public:
+    static HGameObject CreateGameObjFromModel(const std::string& _name,
+                                              const std::string& _modelPath,
+                                              const std::vector<std::string>& _texturePaths);
+
     std::string getAppName() const override;
     Version getAppVersion() const override;
 
